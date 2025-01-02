5651ffa17c93b001052173c79ecd60f92310a647
manit <38018281+Manit-Chanthavong@users.noreply.github.com>
2025-01-02 12:49:59 -0600

    update to .net 9

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-cp -r net-core-orm/Views/tsnode-mysql-collections net-core-orm/bin/Debug/net8.0/Views
-cd net-core-orm/bin/debug/net8.0
+cp -r net-core-orm/Views/tsnode-mysql-collections net-core-orm/bin/Debug/net9.0/Views
+cd net-core-orm/bin/debug/net9.0
 ./net-core-orm
 
 
